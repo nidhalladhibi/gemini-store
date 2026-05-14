9dc900c2146b08d12731e0aa7472054a0055cfa6
Nidhal <www.nidhal.com@email.com>
2026-05-14 16:23:14 +0100

    modification

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,8 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "wiki.tn" },
-      { protocol: "https", hostname: "www.mitunisie.tn" }
+      { protocol: "https", hostname: "www.mitunisie.tn" },
+      { protocol: "https", hostname: "www.mytek.tn" }
     ]
   }
 };
